e116b19e25c9a063344056154625f7a16bf97295
渡辺千華 <watanabechika@Einstein.local>
2019-02-26 10:48:34 +0900

    算出プロパティとウォッチャまで

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = new Vue({
 var app2 = new Vue({
   el: '#app-2',
   data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
+    message: 'You loaded this page on ' + new Date().toLocaleString() //この要素の title 属性を Vue インスタンスの message プロパティによって更新して保存する
   }
 })
 var app3 = new Vue({
@@ -45,3 +45,46 @@ var app6 = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+Vue.component('todo-item' ,{
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat'}
+    ]
+  }
+})
+
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
+
+
+computed: {
+  fullName: {
+    get: function () {
+      return this.firstName + ' ' + this.lastName
+    },
+    set: function(newValue){
+      var names = newValue.spilit(' ')
+      this.firstName = names[0]
+      this.lastName = names[names.length - 1]
+    }
+  }
+}
